8a4fe14dba1bdc222b6b1e257398159b01a6f0c0
Choenden Kyirong <kyirong6@gmail.com>
2018-01-08 14:11:33 -0800

    Update int_queue.h

--- Lectures/Lecture 3/int_queue.h
+++ Lectures/Lecture 3/int_queue.h
@@ -1,14 +1,14 @@
 class int_queue {
   public:
-	   int_queue();
-	   void enqueue(int item);
-	   int dequeue();
-     int size();
-     bool empty();
+	int_queue();
+        void enqueue(int item);
+        int dequeue();
+        int size();
+        bool empty();
   private:
-	   int capacity;
-	   int *Array;
-	   int front;
-	   int rear;
-	   int qSize;
+	int capacity;
+	int *Array;
+	int front;
+	int rear;
+	int qSize;
 };
