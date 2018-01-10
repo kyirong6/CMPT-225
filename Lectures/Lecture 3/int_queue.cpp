38e0fc8a261cf95e136aa5137e8b9dd533f2472f
Choenden <kyirong6@gmail.com>
2018-01-10 14:14:41 -0800

    some changes

--- Lectures/Lecture 3/int_queue.cpp
+++ Lectures/Lecture 3/int_queue.cpp
@@ -1,5 +1,12 @@
 #include "int_queue.h"
 
+/* File: int_queue.cpp
+
+  Implementation of functions for basic "circular-array" implementation of a Queue.
+
+*/
+
+
 int_queue::int_queue() {
 	capacity=100;
 	A =  new int[capacity];
@@ -11,3 +18,21 @@ int_queue::int_queue() {
 int int_queue::size() { return qSize;}
 
 bool int_queue::empty(){return qSize==0;}
+
+int Basic_int_queue::front(){
+  		return A[front_index];
+	}
+
+int Basic_int_queue::dequeue(){
+  int item = A[front_index];
+  front_index = ( front_index + 1 ) % capacity ;
+  current_size -= 1 ;
+  return item ;
+}
+
+void Basic_int_queue::enqueue( int item ){
+  A[rear_index] = item ;
+  rear_index = ( rear_index + 1 ) % capacity ;
+  current_size += 1 ;
+}
+
