a652e4d4cb33ef9721bbbf9649ca77a7e01dfd58
Choenden <kyirong6@gmail.com>
2018-03-10 15:09:59 -0800

    implemented pop methods for deque

--- Assignment2/deque.cpp
+++ Assignment2/deque.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Deque::Deque() {
-	left_p = rear_p = curor = NULL;
+	left_p = rear_p = cursor = NULL;
 	current_size = 0;
 }
 
@@ -26,6 +26,42 @@ void Deque::push_right(int item) {
 		left_p->prev_node = new_rear;
 		rear_p = new_rear;
 	}
+	current_size++;
 }
+
+int Deque::pop_left() {
+	Node *tmp = left_p
+	int val = left_p->data;
+	// there is only one element in deque
+	if ((left_p->next_node == NULL) & (left_p->prev_node == NULL))
+		left_p = rear_p = NULL;
+		delete tmp;
+		return val;
+	else {
+	left_p->prev_node->next_node = NULL;
+	left_p = left_p->prev_node;
+	}
+	delete tmp;
+	current_size--;	
+	return val;
+}
+
+int Deque::pop_right() {
+	Node *tmp = rear_p;
+	int val = rear_p->data;
+	//there is only one element in deque
+	if ((left_p->next_node == NULL) & (left_p->prev_node == NULL))
+		left_p = rear_p = NULL;
+		delete tmp;
+		return val;
+	else {
+		rear_p->next_node->prev_node = NULL;
+		rear_p = rear_p->next_node;
+	}
+	delete tmp;
+	current_size--;
+	return val;
+}
+
 
 
