1bd601b430500c07429439850b40fc93243c279d
Choenden <kyirong6@gmail.com>
2018-03-10 14:41:37 -0800

    added push methods for deque

--- Assignment2/deque.cpp
+++ Assignment2/deque.cpp
@@ -8,5 +8,24 @@ Deque::Deque() {
 }
 
 void Deque::push_left(int item) {
-	
+	if (empty())
+		left_p = rear_p = Node *node(item, NULL, NULL);
+	else {
+		Node * new_left(item, left_p, NULL);
+		left_p->next_node = new_left;
+		left_p = new_left;
+	}
+	current_size++;
 }
+
+void Deque::push_right(int item) {
+	if (empty())
+		left_p = rear_p = Node *node(item, NULL, NULL);
+	else {
+		Node new_rear(item, NULL, rear_p);
+		left_p->prev_node = new_rear;
+		rear_p = new_rear;
+	}
+}
+
+
