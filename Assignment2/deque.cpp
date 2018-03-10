bfdbdb3787f64ebc9ea9e6c07db44ec57d4e7b99
Choenden <kyirong6@gmail.com>
2018-03-10 15:23:39 -0800

    added some cursor implementation and a variety of methods

--- Assignment2/deque.cpp
+++ Assignment2/deque.cpp
@@ -10,10 +10,12 @@ Deque::Deque() {
 void Deque::push_left(int item) {
 	if (empty())
 		left_p = rear_p = Node *node(item, NULL, NULL);
+		cursor = left_p;
 	else {
 		Node * new_left(item, left_p, NULL);
 		left_p->next_node = new_left;
 		left_p = new_left;
+		cursor = left_p;
 	}
 	current_size++;
 }
@@ -23,7 +25,7 @@ void Deque::push_right(int item) {
 		left_p = rear_p = Node *node(item, NULL, NULL);
 	else {
 		Node new_rear(item, NULL, rear_p);
-		left_p->prev_node = new_rear;
+		rear_p->prev_node = new_rear;
 		rear_p = new_rear;
 	}
 	current_size++;
@@ -34,12 +36,13 @@ int Deque::pop_left() {
 	int val = left_p->data;
 	// there is only one element in deque
 	if ((left_p->next_node == NULL) & (left_p->prev_node == NULL))
-		left_p = rear_p = NULL;
+		left_p = rear_p = cursor = NULL;
 		delete tmp;
 		return val;
 	else {
 	left_p->prev_node->next_node = NULL;
 	left_p = left_p->prev_node;
+	cursor = left_p;
 	}
 	delete tmp;
 	current_size--;	
@@ -62,6 +65,41 @@ int Deque::pop_right() {
 	current_size--;
 	return val;
 }
+
+bool Deque::size() {
+	return current_size;
+}
+
+bool Deque::empty() {
+	return current_size == 0;
+}
+
+bool Deque::cursor_left() {
+	if (empty())
+		return false;
+	if else(cursor->next_node == NULL)
+		return false;
+	cursor = cursor->next_node
+	return true;
+}
+
+bool Deque::cursor_right() {
+	if (empty())
+		return false;
+	if else(cursor->prev_node == NULL)
+		return false;
+	cursor = cursor->prev_node;
+	return true;
+}
+
+void Deque::peak_left() {
+	cout << left_p->data << endl;
+}
+
+void Deque::peak_right() {
+	cout << right_p->data << endl;
+}
+
 
 
 
