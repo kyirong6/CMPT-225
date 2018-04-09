c7f9aa57234e0ce704b11b11dde5827991b9f914
Choenden <kyirong6@gmail.com>
2018-04-09 13:47:53 -0700

    modified assignment 2 and added lecture 17

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,4 +1,4 @@
-/* Deque Test Program 4 */
+/* Deque Test Program 3 */
 #include <cstring>
 #include <iostream>
 #include "Deque.h"
@@ -6,62 +6,29 @@
 using namespace std ;
 
 int main (int argc, char * const argv[]) {
-    cout << "\n\nDeque Class Test Program 4 - START\n\n";
+    cout << "\n\nDeque Class Test Program 3 - START\n\n";
 
     // Make a Deque
-    Deque dq1;
+    Deque * dq1 = new Deque();
+    dq1->display();
 
     // Insert some elements 
-    dq1.push_left(1);
-    dq1.push_left(2);
-    dq1.push_left(3);
-    dq1.push_left(4);
-    dq1.push_left(5);
-    dq1.push_left(6);
-    dq1.push_left(7);
-    dq1.push_left(8);
-    dq1.cursor_left();
-    dq1.cursor_left();
-    dq1.cursor_left();
-    dq1.verbose_display();
+    dq1->push_left(1);
+    dq1->push_left(2);
+    dq1->push_left(3);
+    dq1->push_left(4);
+    dq1->push_left(5);
+    dq1->push_left(6);
+    dq1->push_left(7);
+    dq1->push_left(8);
+    dq1->display();
+    // add 10 to each 
+    int i = 0 ;
+    do{
+        dq1->set_cursor( 10 + dq1->get_cursor());
+	dq1->display();
+    }while( dq1->cursor_left());
 
-
-    // Make a copy
-    Deque dq2(dq1);
-    dq1.verbose_display();
-
-    // Modify the original and the copy differently;
-    dq1.cursor_left();
-    dq1.set_cursor(8);
-    dq2.cursor_right();
-    dq2.set_cursor(9);
-    dq1.verbose_display();
-    dq2.verbose_display();
-
-    // Make a Deque
-    Deque * dq3 = new Deque();
-
-    // Insert some elements 
-    dq3->push_left(1);
-    dq3->push_left(2);
-    dq3->push_left(3);
-    dq3->push_left(4);
-    dq3->push_left(5);
-    dq3->cursor_left();
-    dq3->cursor_left();
-    dq3->verbose_display();
- 
-    // Make a copy
-    Deque * dq4 = new Deque(*dq3);
-    dq4->verbose_display();
-
-    // Modify the original and the copy differently;
-    dq3->cursor_left();
-    dq3->set_cursor(8);
-    dq4->cursor_right();
-    dq4->set_cursor(9);
-    dq3->verbose_display();
-    dq4->verbose_display();
-
+    cout << "\n\nDeque Class Test Program 3 - DONE\n\n";
     return 0;
 }
